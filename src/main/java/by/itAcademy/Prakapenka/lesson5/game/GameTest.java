3da70a5e718372fa040044cf4867b0754eed4425
HlebPrakapenka <glebprokop@gmail.com>
2021-11-07 18:45:36 +0300

    Lesson 5 game corrected

--- src/main/java/by/itAcademy/Prakapenka/lesson5/game/GameTest.java
+++ src/main/java/by/itAcademy/Prakapenka/lesson5/game/GameTest.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 
 public class GameTest {
     public static void main(String[] args) {
-        //drowFieldTest();
+        drowFieldTest();
         userMakeMoveTest();
-        //isGameFinishTest();
+        isGameFinishTest();
     }
 
     public static void drowFieldTest(){
