3ac53e240ef5ef89ac039179fb4ebea92e56146c
glebprokop <57326827+glebprokop@users.noreply.github.com>
2021-12-03 00:27:41 +0300

    Lesson 14 (files task)

---
+++ src/main/java/by/itAcademy/Prakapenka/lesson14/Application.java
@@ -0,0 +1,46 @@
+package main.java.by.itAcademy.Prakapenka.lesson14;
+
+import java.io.*;
+
+public class Application
+{
+    public static void main(String[] args) {
+        String fileName = "D:/file.txt";
+        copyFile(fileName);
+
+        String printedWay = "D:/Учебка"; // путь для примера работы
+        printDirectories(printedWay);
+    }
+
+    //additional task
+    public static void printDirectories(String printedWay) {
+        File file = new File(printedWay);
+
+        if (file.isDirectory()){
+            System.out.println("DIRECTORY: " + file);
+            for (String f : file.list()) {
+                printDirectories(printedWay + "/" + f);
+            }
+        }else{
+            System.out.println("File: " + file.getName());
+        }
+    }
+
+    //main task
+    public static void copyFile(String file){
+        try (Reader myReader = new FileReader(file);
+             BufferedReader myBufReader = new BufferedReader(myReader)) {
+             String nextStr;
+
+             BufferedWriter myBufWriter = new BufferedWriter(new FileWriter("copiedFile.txt"));
+
+             while ((nextStr = myBufReader.readLine()) != null) {
+                 System.out.println(nextStr);
+                 myBufWriter.write(nextStr + "\n");
+                 myBufWriter.flush();
+             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
