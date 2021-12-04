3dc615563b93a7eea02eb131d6689b5a7ec46e02
glebprokop <57326827+glebprokop@users.noreply.github.com>
2021-12-04 19:41:17 +0300

    Lesson 14 (files task - edited)

--- src/main/java/by/itAcademy/Prakapenka/lesson14/Application.java
+++ src/main/java/by/itAcademy/Prakapenka/lesson14/Application.java
@@ -5,10 +5,10 @@ import java.io.*;
 public class Application
 {
     public static void main(String[] args) {
-        String fileName = "../fil.txt";
+        String fileName = "../fil.txt"; //относительный путь
         copyFile(fileName);
 
-        String printedWay = "."; // путь для примера работы
+        String printedWay = ".";
         printDirectories(printedWay);
     }
 
