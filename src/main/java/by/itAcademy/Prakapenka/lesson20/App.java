70908a6ea999a22a09a06d30b56bd4b1d567d716
Глеб <glebprokop@gmail.com>
2022-01-04 16:39:35 +0300

    Lesson 20 (исправлено)

--- src/main/java/by/itAcademy/Prakapenka/lesson20/App.java
+++ src/main/java/by/itAcademy/Prakapenka/lesson20/App.java
@@ -1,19 +1,29 @@
 package main.java.by.itAcademy.Prakapenka.lesson20;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 public class App {
     public static void main(String[] args) {
 
         WorkClass newWC = new WorkClass();
-        Method[] methods = newWC.getClass().getDeclaredMethods();
+        Class checkerClass = newWC.getClass();
+        Class checkingAnn = AcademyInfo.class;
 
-        for(Method method : methods){
-            if (method.isAnnotationPresent(AcademyInfo.class)){
-                System.out.println("Method " + method.getName() + " in class " + method.getDeclaringClass() +
-                        " has our annotation with field " + method.getAnnotation(AcademyInfo.class).year());
+        // Вывод значения boolean для определения наличия передаваемой аннотации
+        System.out.println(isPresentAnnotation(checkerClass, checkingAnn));
+    }
+
+    public static boolean isPresentAnnotation(Class clazz, Class annotation) {
+        Method[] methods = clazz.getDeclaredMethods();
+
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(annotation)) {
+                return true;
             }
         }
+
+        return false;
     }
 }
 
