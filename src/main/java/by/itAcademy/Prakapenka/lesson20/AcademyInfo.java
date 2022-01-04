36bb59684f1950973fe011d450b6c519a30e25f6
Глеб <glebprokop@gmail.com>
2022-01-04 16:39:24 +0300

    Lesson 20 (исправлено)

--- src/main/java/by/itAcademy/Prakapenka/lesson20/AcademyInfo.java
+++ src/main/java/by/itAcademy/Prakapenka/lesson20/AcademyInfo.java
@@ -5,7 +5,7 @@ import java.lang.annotation.*;
 /**
  * Создание аннотации AcademyInfo. Цель - для пометки методов, время жизни - всегда (в том числе в Runtime).
  */
-//@Target(value=ElementType.METHOD)
+@Target(value=ElementType.METHOD)
 @Retention(value= RetentionPolicy.RUNTIME)
 public @interface AcademyInfo {
     String year();
